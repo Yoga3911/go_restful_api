1c0c94af835257a7d5108b3ec81f52d08fd4aaa1
Yoga3911 <yoga3911@gmail.com>
2021-12-26 08:39:42 +0700

    add middleware

---
+++ middleware/jwt-auth.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"pkg/coba/entity"
+	"pkg/coba/helper"
+	"pkg/coba/service"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/nacl/auth"
+)
+
+// AutthorizeJWT validates the token user given, return 401 if not valid
+func AuthorizeJWT(jwtService service.JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			response := helper.BuildErrorResponse("Failed to process request", "No token found", nil)
+			c.AbortWithStatusJSON(http.StatusBadRequest, response)
+			return
+		}
+		token, err := jwtService.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id] :", claims["user_id"])
+			log.Println("Claim[issuer] :", claims["issuer"])
+		} else {
+			log.Println(err)
+			response := helper.BuildErrorResponse("Token is not valid", err.Error(), nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+	}
+}
